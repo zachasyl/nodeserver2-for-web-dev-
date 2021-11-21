457765c28d222583625eed5c033821ccebc62f0c
zachasyl <sylvane.z@northeastern.edu>
2021-11-21 17:14:47 -0500

    port update

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get('/hello', (req, res) => {
 // require('./services/movies-service')(app);
 // require('./services/tweeter-service')(app);
 
-app.listen(process.env.PORT || 4000, ...);
+app.listen(process.env.PORT || 4000);
